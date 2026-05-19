3bf8503e61c243a7ea4bbb92f637ed595ce7913b
Puzzl3Mak3r <tahaandshaik@gmail.com>
2026-05-19 15:09:29 +1000

    sample code to see if it works

--- pass/pass.ino
+++ pass/pass.ino
@@ -2,13 +2,17 @@
 
 // Pin Definitions
 
+// Variables
+// const char myAddress = '0'; // Default factory address for SDI-12 sensors
+// const String commandBuffer = "";  // Buffer to hold incoming UART characters
+
 // Main Functions
-void setup()
-{
+void setup() {
   Serial.begin(9600);
+  
+  Serial.println("--- SDI-12 Environmental Sensor Node Booting ---");
+  Serial.println("Type '?!', '0I!', '0M!', or '0D0!' to test.");
 }
 
-void loop()
-{
-
+void loop() {
 }
