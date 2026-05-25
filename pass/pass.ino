717cf399926b6ac7c1fcbc5413c7373fb2e8510b
Jaymond <jmart384@gmail.com>
2026-05-25 18:06:09 +1000

    Revert "Update pass.ino"
    This reverts commit c3027b51c823f7d3efa4e3c03556dc2733ba5675.

--- pass/pass.ino
+++ pass/pass.ino
@@ -93,6 +93,7 @@ void loop() {
       // Pass the complete command string to the parser
       handleCommand(command + "!");
       
+      // Wipe the buffer clean immediately so it's ready for the next command
       command = ""; 
     } else {
       // Ignore start bits (0) and append valid characters to the buffer
